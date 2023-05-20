3edcaabfa7bfb2e8ee644dcd285cb61e40961be5
Carlos Soto <carloss.empire@gmail.com>
2023-05-20 13:25:43 -0300

    Operador ternario en linea 41 para agregar icono de vivo o muerto en cada personaje segun su estatus

--- src/index.js
+++ src/index.js
@@ -25,24 +25,31 @@ setTimeout(() => {
          * Creacion de card por cada personaje listado
          */
         results.forEach(character => {
-            console.log(character);
             const article = document.createElement('article');
             article.setAttribute('class', 'card');
             article.innerHTML = `
                 <picture>
                     <img class="card__img" src="${character.image}" alt="Imagen de ${character.name}">
                 </picture>
-                <div>
+                <div class="card__info">
                     <h4 class="card__tittle">${character.name}</h4>
-                    <div class="card__info">
-                        <p class="card__text"><iconify-icon icon="pajamas:status-health"></iconify-icon>${character.status}</p>
-                        <p class="card__text"><iconify-icon icon="ph:alien"></iconify-icon>${character.species}</p>
-                        <p class="card__text"><iconify-icon icon="ph:planet"></iconify-icon>${character.origin.name}</p>
+                    <p id="${character.id}-icon" class="card__text">
+                    ${
+                    /**
+                     * Icono de vida o muerte segun estatus del personaje
+                     */
+                    character.status.toLowerCase() == 'alive' || character.status.toLowerCase() == 'unknown' 
+                    ? '<iconify-icon class="status__icon" icon="pajamas:status-health"></iconify-icon>' 
+                    : '<iconify-icon class="status__icon--death" icon="streamline:interface-edit-skull-1-crash-death-delete-die-error-garbage-remove-skull-trash"></iconify-icon>'}
+                    ${character.status}</p>
+                    <p class="card__text"><iconify-icon icon="ph:alien"></iconify-icon>${character.species}</p>
+                    <p class="card__text"><iconify-icon icon="ph:planet"></iconify-icon>${character.origin.name}</p>
                     </div>
                 </div>
                 <a class="card__link" href="/character?id=${character.id}"><iconify-icon icon="ph:info"></iconify-icon>Ver detalles</a>
             `
-            characters.appendChild(article)
+
+            characters.appendChild(article);
         });
     }
     
